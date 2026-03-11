1d35eefc495bd13c7b25e7717e46a0e1c9fc0e23
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-03-11 10:17:58 +0600

    Programmatic Navigation using useRouter from next/navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-import Link from "next/link";
+"use client"
+// import Link from "next/link";
+
+import { useRouter } from "next/navigation";
 
 const HomePage = () => {
+  const router = useRouter();
+
+  const handleNavigation = () => {
+    router.push("/dashboard")
+  }
   return (
     <div>
       <h1 className="text-4xl">Home Page</h1>
-      <Link href="/dashboard">
+
+      {/* <Link prefetch={false} href="/dashboard">
         <button className="bg-green-500 px-2 cursor-pointer">Dashboard</button>
-      </Link>
+      </Link> */}
+
+      {/* <Link href="/dashboard">
+        <button className="bg-green-500 px-2 cursor-pointer">Dashboard</button>
+      </Link> */}
+
+      <button onClick={handleNavigation} className="bg-green-500 px-2 cursor-pointer">Dashboard</button>
+
     </div>
   );
 };
