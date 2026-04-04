6c7c4b23d90994f73fbafa95387138b30033e868
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-04-04 13:27:24 +0600

    52-1 Optimize image using Image Component

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // images: {
+  //   remotePatterns: [
+  //     new URL("https://res.cloudinary.com/dbcykhgzd/image/upload/v1770562999/7iwiolo8t34-1770562995708-loginpage.png.png"),
+  //   ],
+  // }
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import myImage from "../../assets/images/mithun.png"
+
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1 className="text-center">Regular img tag</h1>
+      <img
+        src="https://res.cloudinary.com/dbcykhgzd/image/upload/v1770562999/7iwiolo8t34-1770562995708-loginpage.png.png"
+        alt=""
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+
+      <h1 className="text-center mt-5">Next.js Image Component</h1>
+      <Image
+        src="https://res.cloudinary.com/dbcykhgzd/image/upload/v1770562999/7iwiolo8t34-1770562995708-loginpage.png.png"
+        alt="Next.js Image"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+
+      <h1 className="text-center mt-5">Next.js Image Component with local image from app/assets/images</h1>
+      <Image
+        src={myImage}
+        alt="My Image"
+        className="mx-auto"
+
+      />
+
+      <h1 className="text-center mt-5">Next.js Image Component with local image from public</h1>
+      <Image
+        src="/mithun.JPG"
+        alt="My Image from public"
+        width={300}
+        height={300}
+        className="mx-auto"
+
+      />
+    </div>
+  );
+};
+
+export default GalleryPage;
