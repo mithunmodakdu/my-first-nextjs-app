3501629e237ac300fd215435591d3e45e6ec28d3
Mithun4450 <132843404+Mithun4450@users.noreply.github.com>
2026-04-19 15:09:27 +0600

    Advance Data Fetching Strategies with SSR and loading

---
+++ src/app/products/loading.tsx
@@ -0,0 +1,9 @@
+const ProductsLoadingPage = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-5xl">Loading...</h1>
+    </div>
+  );
+};
+
+export default ProductsLoadingPage;
